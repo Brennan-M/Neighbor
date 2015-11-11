c88677aa3b33c6fbf0fce48960159fb53e9b7936
Brennan McConnell <brmc3887@colorado.edu>
2015-11-11 00:22:31 -0700

    Working on adding UI for functionality

--- Application/Neighbor/app/src/main/java/com/csci4448/android/neighbor/EditProfileActivity.java
+++ Application/Neighbor/app/src/main/java/com/csci4448/android/neighbor/EditProfileActivity.java
@@ -1,25 +1,25 @@
 package com.csci4448.android.neighbor;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.parse.ParseUser;
 
-public class HomescreenActivity extends AppCompatActivity {
+public class EditProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_homescreen);
+        setContentView(R.layout.activity_edit_profile);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_homescreen, menu);
+        getMenuInflater().inflate(R.menu.menu_edit_profile, menu);
         return true;
     }
 
@@ -31,14 +31,12 @@ public class HomescreenActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
+        //noinspection SimplifiableIfStatement
         if (id == R.id.sign_out) {
             ParseUser.logOut();
 
             Intent intent = new Intent(this, MainActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-        } else if (id == R.id.edit_profile) {
-            Intent intent = new Intent(this, EditProfileActivity.class);
             startActivity(intent);
         }
 
