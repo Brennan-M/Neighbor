d368a4a9ea00194b52bac6eaef7a7e823aeba311
Brennan McConnell <brmc3887@colorado.edu>
2015-12-02 21:55:04 -0700

    Made the neighbor user more of an OO class which now holds the responsibilitiy instead of the activities

--- Application/Neighbor/app/src/main/java/com/csci4448/android/neighbor/NeighborUser.java
+++ Application/Neighbor/app/src/main/java/com/csci4448/android/neighbor/NeighborUser.java
@@ -1,12 +1,19 @@
 package com.csci4448.android.neighbor;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseRelation;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 /**
  * Created by Brennan on 12/1/15.
  */
-public class NeighborUser {
-
+public class NeighborUser extends AppCompatActivity {
 
     private static NeighborUser currentUser = null;
     private static ParseUser pUser;
@@ -25,5 +32,57 @@ public class NeighborUser {
 
     public ParseUser getParseUser() {
         return pUser;
+    }
+
+    public void postItem(Context mContext) {
+        Intent intent = new Intent(mContext, PostItemActivity.class);
+        mContext.startActivity(intent);
+    }
+
+    public void searchItems(Context mContext, String query) {
+        Intent intent = new Intent(mContext, SearchActivity.class);
+        intent.putExtra("Query", query);
+        mContext.startActivity(intent);
+    }
+
+    public void viewItemsOwned(Context mContext) {
+        Intent intent = new Intent(mContext, ItemsOwnedActivity.class);
+        mContext.startActivity(intent);
+    }
+
+    public void viewItemsRented(Context mContext) {
+        Intent intent = new Intent(mContext, ItemsRentedActivity.class);
+        mContext.startActivity(intent);
+    }
+
+    public void editProfile(Context mContext) {
+        Intent intent = new Intent(mContext, EditProfileActivity.class);
+        mContext.startActivity(intent);
+    }
+
+    public void rentItem(final Context mContext, RentalItem post) {
+
+        RentalNotification newNotification = new RentalNotification();
+        post.setRenter(pUser);
+        ParseRelation itemsRented = pUser.getRelation("ItemsRented");
+        itemsRented.add(post);
+
+        post.saveInBackground(new SaveCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    pUser.saveInBackground();
+                    Intent intent = new Intent(mContext, HomescreenActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    mContext.startActivity(intent);
+                } else {
+                    Toast.makeText(mContext, "Could not rent this item, error occured..", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        newNotification.setItem(post);
+        newNotification.setFrom(pUser);
+        newNotification.setTo(post.getOwner());
+        newNotification.submitRentalNotification();
     }
 }
