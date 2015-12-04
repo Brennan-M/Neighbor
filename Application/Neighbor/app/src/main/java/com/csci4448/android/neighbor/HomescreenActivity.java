98a0ae49293c9f4c87037ddaa9b3c33cb6ac60bc
nnistler <neil.nistler>
2015-12-03 18:50:03 -0700

    Rounded buttons, added magnifying glass image button and added gradient button color change on click functionality

--- Application/Neighbor/app/src/main/java/com/csci4448/android/neighbor/HomescreenActivity.java
+++ Application/Neighbor/app/src/main/java/com/csci4448/android/neighbor/HomescreenActivity.java
@@ -10,6 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
